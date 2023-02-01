a9944491573a0289cc39c9d976f96144dc4bff0a
IBilalShabiir <mianbilal.4288@gmail.com>
2023-02-01 12:26:38 +0500

    clean up

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,19 @@
 import Aboutcard from './Aboutcard.jsx'
 import Footer from './Footer.jsx'
 import Header from './Header.jsx'
+import HomepageAboutSection from './HomepageAboutSection.jsx'
+import HomepageBanner from './HomepageBanner.jsx'
 import Navlink from './Navlink.jsx'
 import Tokencard from './Tokencard.jsx'
+import TokenizationHomepage from './TokenizationHomepage.jsx'
 
 export {
   Aboutcard,
   Footer,
   Header,
+  HomepageAboutSection,
+  HomepageBanner,
   Navlink,
-  Tokencard
+  Tokencard,
+  TokenizationHomepage
 }
