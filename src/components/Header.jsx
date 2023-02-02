e74476e660e0d964f29f6bdc6ae2c502e65ef8ab
Asim Rasheed <asimwarraich533@gmail.com>
2023-02-02 12:34:12 +0500

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,45 @@
+import { Close } from "./Close";
+import { Menu } from "./Menu";
 import React from "react";
-
 export default function Header() {
-  return <div>Header</div>;
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <div className="nav__bar__wraper">
+      <div className="header__nav__bar__container">
+        <div className="nav__bar__logo">logo</div>
+        <div className="nav__bar__container">
+          <div className="nav__contant__link">Home</div>
+          <div className="nav__contant__link">About</div>
+          <div className="nav__contant__link">
+            <div className="nav__contant__link__tag">Assets</div>
+            <div className="nav__contant__link__icon">__</div>
+          </div>
+          <div className="nav__contant__link">
+            <div className="nav__contant__link__tag">Services</div>
+            <div className="nav__contant__link__icon">__</div>
+          </div>
+          <div className="nav__contant__link">Contact</div>
+          <div className="nav__contant__link__btn">Get Started</div>
+          <div className="nav__contant__link__language">
+            <div className="nav__contant__link__language__tag">ENG</div>
+            <div className="nav__contant__link__language__icon">__</div>
+          </div>
+        </div>
+        <div className="nav__bar__btn__menu">
+          <button
+            className="nac__btn"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            {isOpen ? (
+              <Close size={24} color="#fff" />
+            ) : (
+              <Menu size={24} color="#fff" />
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 import Aboutcard from './Aboutcard.jsx'
+import Close from './Close.jsx'
 import Footer from './Footer.jsx'
 import Header from './Header.jsx'
 import HomepageAboutSection from './HomepageAboutSection.jsx'
 import HomepageBanner from './HomepageBanner.jsx'
 import IndustriesSolution from './IndustriesSolution.jsx'
 import IndustrySolutionCard from './IndustrySolutionCard.jsx'
+import Menu from './Menu.jsx'
 import OurClientsHomepage from './OurClientsHomepage.jsx'
 import OurSpecialistCards from './OurSpecialistCards.jsx'
 import Ourspecialities from './Ourspecialities.jsx'
@@ -15,12 +17,14 @@ import TokenizationHomepage from './TokenizationHomepage.jsx'
 
 export {
   Aboutcard,
+  Close,
   Footer,
   Header,
   HomepageAboutSection,
   HomepageBanner,
   IndustriesSolution,
   IndustrySolutionCard,
+  Menu,
   OurClientsHomepage,
   OurSpecialistCards,
   Ourspecialities,
